24d2cf3ecabdb4591555f2d74950fdcd58efa2fd
gsusha <x.dyachenko@gmail.com>
2021-09-22 20:03:38 +0300

    small fixes

--- js/script.js
+++ js/script.js
@@ -5,3 +5,16 @@ $(document).ready(function() {
 		$('.coming-desktop, .coming-mobile').addClass('hidden');
 	});
 });
+
+
+$(document).ready(function(){
+	$(window).on("scroll",function(){
+		let wn = $(window).scrollTop();
+
+		if(wn > 70){
+			$(".header__menu").addClass("fixed");
+		} else{
+			$(".header__menu").removeClass("fixed");
+		}
+	});
+});
