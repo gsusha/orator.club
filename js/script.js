456e76d516da65a29068473601e2862e774ddcdd
gsusha <x.dyachenko@gmail.com>
2021-09-24 01:27:01 +0300

    i hate it

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	$(window).on("scroll",function(){
 		let wn = $(window).scrollTop();
 
-		if(wn > 70){
+		if(wn > 85){
 			$(".header__menu").addClass("fixed");
 		} else{
 			$(".header__menu").removeClass("fixed");
