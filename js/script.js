00484af06cf878fb1a1ba1c3dc8a4a9296804ded
gsusha <x.dyachenko@gmail.com>
2021-09-23 22:58:31 +0300

    questionable fixes

--- js/script.js
+++ js/script.js
@@ -1,15 +1,6 @@
-/* Нажатие на кнопку "Current website" скрывает блок Coming */
-$(document).ready(function() {
-	$('.coming-desktop__btn, .coming-mobile__btn').click(function(event) {
-		$('.wrapper').addClass('active');
-		$('.coming-desktop, .coming-mobile').addClass('hidden');
-	});
-});
-
-
 $(document).ready(function(){
 	$(window).on("scroll",function(){
-		let wwn = $(window).scrollTop();
+		let wn = $(window).scrollTop();
 
 		if(wn > 70){
 			$(".header__menu").addClass("fixed");
